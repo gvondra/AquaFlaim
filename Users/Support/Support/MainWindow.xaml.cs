592f3269e24a75b22bf8e89b43124fe91644cf38
Greg Vondra <gvondra@gmail.com>
2022-03-05 10:59:36 -0600

    add error window, main window, login window, and home page

---
+++ Users/Support/Support/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AquaFlaim.User.Support
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void CloseCommandBinding_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void GoToPageCommandBinding_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            NavigationService navigationService = navigationFrame.NavigationService;
+            //NavigationService navigationService = NavigationService.GetNavigationService(navigationFrame);
+            navigationService.Navigate(new Uri((string)e.Parameter, UriKind.Relative));
+        }
+
+        private void GoogleLoginMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            GoogleLogin googleLogin = new GoogleLogin();
+            googleLogin.ShowDialog();
+        }
+    }
+}
