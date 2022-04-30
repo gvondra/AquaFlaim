6eb869c07697386374fa25b86f6530508d3e1c12
Greg Vondra <gvondra@gmail.com>
2022-04-30 10:34:24 -0500

    add form type saver implementation

---
+++ Forms/Forms.Core/FormTypeSaver.cs
@@ -0,0 +1,25 @@
+﻿using AquaFlaim.CommonCore;
+using AquaFlaim.Forms.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AquaFlaim.Forms.Core
+{
+    public class FormTypeSaver : IFormTypeSaver
+    {
+        public async Task Create(ISettings settings, IFormType formType)
+        {
+            Saver saver = new Saver();
+            await saver.Save(new TransactionHandler(settings), formType.Create);
+        }
+
+        public async Task Update(ISettings settings, IFormType formType)
+        {
+            Saver saver = new Saver();
+            await saver.Save(new TransactionHandler(settings), formType.Update);
+        }
+    }
+}
--- Forms/Forms.Core/FormsCodeModule.cs
+++ Forms/Forms.Core/FormsCodeModule.cs
@@ -14,6 +14,7 @@ namespace AquaFlaim.Forms.Core
         {
             base.Load(builder);
             builder.RegisterType<FormTypeFactory>().As<IFormTypeFactory>();
+            builder.RegisterType<FormTypeSaver>().As<IFormTypeSaver>();
         }
     }
 }
